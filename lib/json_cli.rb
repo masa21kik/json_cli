c61a9b83f58901afc7c265ef96d03134da09fb00
Masaaki Kikuchi <masa21kik@gmail.com>
2013-11-24 23:39:39 +0900

    add unwind command

--- lib/json_cli.rb
+++ lib/json_cli.rb
@@ -2,4 +2,5 @@ require "json_cli/version"
 
 module JsonCli
   require 'json_cli/join'
+  require 'json_cli/unwind'
 end
---
+++ lib/json_cli/unwind.rb
@@ -0,0 +1,43 @@
+# -*- mode: ruby; coding: utf-8 -*-
+require "multi_json"
+
+module JsonCli
+  class UnwindJson
+    def self.unwind_array(io, unwind_key, opt = {})
+      opt[:out] ||= STDOUT
+      io.each_line do |l|
+        j = MultiJson.load(l.chomp)
+        if j.key?(unwind_key) && j[unwind_key].is_a?(Array)
+          j[unwind_key].each do |v|
+            opt[:out].puts MultiJson.dump(j.merge({unwind_key => v}))
+          end
+        else
+          opt[:out].puts MultiJson.dump(j)
+        end
+      end
+    end
+
+    def self.unwind_hash(io, unwind_key, opt = {})
+      opt[:out] ||= STDOUT
+      opt[:flatten] ||= false
+      opt[:key_label] ||= 'key'
+      opt[:value_label] ||= 'value'
+      io.each_line do |l|
+        j = MultiJson.load(l.chomp)
+        if j.key?(unwind_key) && j[unwind_key].is_a?(Hash)
+          base = j.select{|k,v| k != unwind_key} if opt[:flatten]
+          j[unwind_key].each do |k,v|
+            if opt[:flatten]
+              jj = base.merge({opt[:key_label] => k, opt[:value_label] => v,})
+            else
+              jj = j.merge({unwind_key => {k => v}})
+            end
+            opt[:out].puts MultiJson.dump(jj)
+          end
+        else
+          opt[:out].puts MultiJson.dump(j)
+        end
+      end
+    end
+  end
+end
