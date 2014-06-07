919574c4cd1ba8b324fa6ef4a2b0e31bc51d45a7
masa21kik <masa21kik@gmail.com>
2014-06-07 23:10:40 +0900

    rename variables

--- lib/json_cli/join.rb
+++ lib/json_cli/join.rb
@@ -6,10 +6,10 @@ module JsonCli
   class JoinJson
     def self.left_join(left_io, right_io, join_key, out = STDOUT)
       right = io2hash(right_io, join_key)
-      left_io.each do |l|
-        j = MultiJson.load(l.chomp)
-        j.merge!(right[j[join_key]] || {}) if j.key?(join_key)
-        out.puts MultiJson.dump(j)
+      left_io.each do |line|
+        obj = MultiJson.load(line.chomp)
+        obj.merge!(right[obj[join_key]] || {}) if obj.key?(join_key)
+        out.puts MultiJson.dump(obj)
       end
       out.flush
     end
@@ -20,20 +20,20 @@ module JsonCli
 
     def self.inner_join(left_io, right_io, join_key, out = STDOUT)
       right = io2hash(right_io, join_key)
-      left_io.each do |l|
-        j = MultiJson.load(l.chomp)
-        next if !j.key?(join_key) || !right.key?(j[join_key])
-        out.puts MultiJson.dump(j.merge(right[j[join_key]]))
+      left_io.each do |line|
+        obj = MultiJson.load(line.chomp)
+        next if !obj.key?(join_key) || !right.key?(obj[join_key])
+        out.puts MultiJson.dump(obj.merge(right[obj[join_key]]))
       end
       out.flush
     end
 
     def self.io2hash(io, key)
       hash = {}
-      io.each do |l|
-        j = MultiJson.load(l.chomp)
-        next unless j.key?(key)
-        hash[j[key]] = j.select { |k, _| k != key }
+      io.each do |line|
+        obj = MultiJson.load(line.chomp)
+        next unless obj.key?(key)
+        hash[obj[key]] = obj.select { |_k, _| _k != key }
       end
       hash
     end
--- lib/json_cli/unwind.rb
+++ lib/json_cli/unwind.rb
@@ -6,14 +6,14 @@ module JsonCli
   class UnwindJson
     def self.unwind_array(io, unwind_key, opt = {})
       opt[:out] ||= STDOUT
-      io.each do |l|
-        j = MultiJson.load(l.chomp)
-        if j.key?(unwind_key) && j[unwind_key].is_a?(Array)
-          j[unwind_key].each do |v|
-            opt[:out].puts MultiJson.dump(j.merge(unwind_key => v))
+      io.each do |line|
+        obj = MultiJson.load(line.chomp)
+        if obj.key?(unwind_key) && obj[unwind_key].is_a?(Array)
+          obj[unwind_key].each do |val|
+            opt[:out].puts MultiJson.dump(obj.merge(unwind_key => val))
           end
         else
-          opt[:out].puts MultiJson.dump(j)
+          opt[:out].puts MultiJson.dump(obj)
         end
       end
     end
@@ -22,20 +22,20 @@ module JsonCli
       opt[:out] ||= STDOUT
       opt[:key_label] ||= 'key'
       opt[:value_label] ||= 'value'
-      io.each do |l|
-        j = MultiJson.load(l.chomp)
-        if j.key?(unwind_key) && j[unwind_key].is_a?(Hash)
-          base = j.select { |k, _| k != unwind_key } if opt[:flatten]
-          j[unwind_key].each do |k, v|
+      io.each do |line|
+        obj = MultiJson.load(line.chomp)
+        if obj.key?(unwind_key) && obj[unwind_key].is_a?(Hash)
+          base = obj.select { |_k, _| _k != unwind_key } if opt[:flatten]
+          obj[unwind_key].each do |key, val|
             if opt[:flatten]
-              jj = base.merge(opt[:key_label] => k, opt[:value_label] => v)
+              jj = base.merge(opt[:key_label] => key, opt[:value_label] => val)
             else
-              jj = j.merge(unwind_key => { k => v })
+              jj = obj.merge(unwind_key => { key => val })
             end
             opt[:out].puts MultiJson.dump(jj)
           end
         else
-          opt[:out].puts MultiJson.dump(j)
+          opt[:out].puts MultiJson.dump(obj)
         end
       end
     end
