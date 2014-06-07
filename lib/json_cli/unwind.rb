70133c56fd88b9baf07cca86e4f3f9587659aaaf
masa21kik <masa21kik@gmail.com>
2014-06-07 23:49:29 +0900

    fix reek detections

--- lib/json_cli/unwind.rb
+++ lib/json_cli/unwind.rb
@@ -5,38 +5,47 @@ module JsonCli
   # Unwind JSON class
   class UnwindJson
     def self.unwind_array(io, unwind_key, opt = {})
-      opt[:out] ||= STDOUT
       io.each do |line|
         obj = MultiJson.load(line.chomp)
-        if obj.key?(unwind_key) && obj[unwind_key].is_a?(Array)
-          obj[unwind_key].each do |val|
-            opt[:out].puts MultiJson.dump(obj.merge(unwind_key => val))
-          end
-        else
+        if !obj.key?(unwind_key) || !obj[unwind_key].is_a?(Array)
           opt[:out].puts MultiJson.dump(obj)
+        else
+          unwind_array_obj(obj, unwind_key, opt)
         end
       end
     end
 
+    def self.unwind_array_obj(obj, unwind_key, opt)
+      obj[unwind_key].each do |val|
+        opt[:out].puts MultiJson.dump(obj.merge(unwind_key => val))
+      end
+    end
+
     def self.unwind_hash(io, unwind_key, opt = {})
-      opt[:out] ||= STDOUT
-      opt[:key_label] ||= 'key'
-      opt[:value_label] ||= 'value'
       io.each do |line|
         obj = MultiJson.load(line.chomp)
-        if obj.key?(unwind_key) && obj[unwind_key].is_a?(Hash)
-          base = obj.select { |_k, _| _k != unwind_key } if opt[:flatten]
-          obj[unwind_key].each do |key, val|
-            if opt[:flatten]
-              jj = base.merge(opt[:key_label] => key, opt[:value_label] => val)
-            else
-              jj = obj.merge(unwind_key => { key => val })
-            end
-            opt[:out].puts MultiJson.dump(jj)
-          end
-        else
+        if !obj.key?(unwind_key) || !obj[unwind_key].is_a?(Hash)
           opt[:out].puts MultiJson.dump(obj)
+        elsif opt[:flatten]
+          unwind_hash_obj_flatten(obj, unwind_key, opt)
+        else
+          unwind_hash_obj(obj, unwind_key, opt)
         end
+      end
+    end
+
+    def self.unwind_hash_obj(obj, unwind_key, opt)
+      obj[unwind_key].each do |key, val|
+        jj = obj.merge(unwind_key => { key => val })
+        opt[:out].puts MultiJson.dump(jj)
+      end
+    end
+
+    def self.unwind_hash_obj_flatten(obj, unwind_key, opt)
+      base = obj.select { |key, _| key != unwind_key }
+      obj[unwind_key].each do |key, val|
+        jj = base.merge(opt[:key_label] => key, opt[:value_label] => val)
+        opt[:out].puts MultiJson.dump(jj)
       end
     end
   end
