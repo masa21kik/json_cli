7f47db442d492466be9710cfab575ad15c6ce28f
masa21kik <masa21kik@gmail.com>
2014-06-09 23:44:51 +0900

    refactor cli

--- lib/json_cli/cli.rb
+++ lib/json_cli/cli.rb
@@ -10,10 +10,7 @@ module JsonCli
     option :join_file, required: true,  aliases: :j
     option :base_file, aliases: :b, default: '/dev/stdin'
     def left_join
-      left_io = File.open(options[:base_file], 'r')
-      right_io = File.open(options[:join_file], 'r')
-      JsonCli::Command::Join.new(left_io, right_io, opts).left_join
-      [left_io, right_io, opts[:out]].each(&:close)
+      join_common(__method__)
     end
 
     desc 'right_join', 'right outer join of json files'
@@ -21,10 +18,7 @@ module JsonCli
     option :join_file, required: true,  aliases: :j
     option :base_file, aliases: :b, default: '/dev/stdin'
     def right_join
-      left_io = File.open(options[:base_file], 'r')
-      right_io = File.open(options[:join_file], 'r')
-      JsonCli::Command::Join.new(left_io, right_io, opts).right_join
-      [left_io, right_io, opts[:out]].each(&:close)
+      join_common(__method__)
     end
 
     desc 'inner_join', 'inner join of json files'
@@ -32,18 +26,13 @@ module JsonCli
     option :join_file, required: true,  aliases: :j
     option :base_file, aliases: :b, default: '/dev/stdin'
     def inner_join
-      left_io = File.open(options[:base_file], 'r')
-      right_io = File.open(options[:join_file], 'r')
-      JsonCli::Command::Join.new(left_io, right_io, opts).inner_join
-      [left_io, right_io, opts[:out]].each(&:close)
+      join_common(__method__)
     end
 
     desc 'unwind_array [JSON_FILE]', 'unwind array of json file'
     option :unwind_key, required: true, aliases: :k
     def unwind_array(json_file = '/dev/stdin')
-      io = File.open(json_file, 'r')
-      JsonCli::Command::Unwind.new(io, opts).unwind_array
-      [io, opts[:out]].each(&:close)
+      unwind_common(__method__, json_file)
     end
 
     desc 'unwind_hash [JSON_FILE]', 'unwind hash of json file'
@@ -52,12 +41,23 @@ module JsonCli
     option :key_label
     option :value_label
     def unwind_hash(json_file = '/dev/stdin')
-      io = File.open(json_file, 'r')
-      JsonCli::Command::Unwind.new(io, opts).unwind_hash
-      [io, opts[:out]].each(&:close)
+      unwind_common(__method__, json_file)
     end
 
     private
+
+    def join_common(command)
+      left_io = File.open(options[:base_file], 'r')
+      right_io = File.open(options[:join_file], 'r')
+      JsonCli::Command::Join.new(left_io, right_io, opts).send(command)
+      [left_io, right_io, opts[:out]].each(&:close)
+    end
+
+    def unwind_common(command, json_file)
+      io = File.open(json_file, 'r')
+      JsonCli::Command::Unwind.new(io, opts).send(command)
+      [io, opts[:out]].each(&:close)
+    end
 
     def opts
       @opts ||= { out: File.open(options[:output], 'w') }
